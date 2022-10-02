a97ff884208fa0349aff25f969954a0caeae025b
Pavel Popov <legeon48@mail.ru>
2022-10-02 17:24:41 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,71 @@
+import Matrix.Matrix;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner in = new Scanner(System.in);
+        var menu = 0;
+
+        int row, col;
+        System.out.print("\nВведите кол-во строк в матрице:");
+        row = in.nextInt();
+        System.out.print("\nВведите кол-во колонок в матрице:");
+        col = in.nextInt();
+        Matrix matrix1 = new Matrix(row, col);
+        matrix1.fillManually();
+
+        Matrix matrix2 = new Matrix(row,col);
+        matrix2.automaticFilling(100);
+
+        System.out.println("Matrix1:");
+        matrix1.print();
+        System.out.println("\nMatrix2:");
+        matrix2.print();
+        System.out.println("""
+                Выбирете действие:
+                сложение - 1
+                вычетание - 2
+                умножение - 3
+                max число - 4
+                min число - 5
+                среднеарифметическое - 6""");
+        menu = in.nextInt();
+
+        if(menu == 1){
+            Matrix res = new Matrix(row,col);
+            res = matrix1.sum(matrix2);
+            System.out.println("Result:");
+            res.print();
+        }
+        else if (menu == 2) {
+            Matrix res = new Matrix(row,col);
+            res = matrix1.subtraction(matrix2);
+            System.out.println("Result:");
+            res.print();
+        }
+        else if(menu == 3) {
+            Matrix res = new Matrix(row,col);
+            res = matrix1.multiply(matrix2);
+            System.out.println("Result:");
+            res.print();
+        }
+        else if(menu == 4) {
+            System.out.println("Matrix1:");
+            matrix1.print();
+            System.out.println(matrix1.maxNamber());
+        }
+        else if(menu == 5) {
+            System.out.println("Matrix1:");
+            matrix1.print();
+            System.out.println(matrix1.minNamber());
+        }
+        else if(menu == 6) {
+            System.out.println("Matrix1:");
+            matrix1.print();
+            System.out.println(matrix1.arithmeticMean());
+        }
+
     }
 }
--- src/Matrix/Matrix.java
+++ src/Matrix/Matrix.java
@@ -1,2 +1,152 @@
-package PACKAGE_NAME;public class Matrix {
+package Matrix;
+
+import java.util.Scanner;
+
+public class Matrix {
+
+    private final int rows;
+    private final int columns;
+    private double[][] matrix;
+
+
+    public Matrix() {
+        this.rows    = 10;
+        this.columns = 10;
+        this.matrix  = new double[rows][columns];
+    }
+
+    public Matrix(int rows, int columns){
+
+        if(rows <= 0 || columns <= 0) {
+           System.out.println("Заданы неверные размеры матрицы");
+        }
+
+        this.rows    = rows;
+        this.columns = columns;
+        this.matrix  = new double[rows][columns];
+    }
+    public Matrix(double[][] matrix) {
+        this.rows    = matrix.length;
+        this.columns = matrix[0].length;
+        this.matrix  = matrix;
+    }
+
+    private int getSizeRows(double[][] matrix) {return matrix.length;}
+    private int getSizeColumns(double[][] matrix) {return matrix[0].length;}
+    private boolean comparison(Matrix matrix)
+    {
+        return this.rows == matrix.rows && this.columns == matrix.columns;
+    }
+    private int rnd(double max)
+    {
+        return (int) (Math.random() * max);
+    }
+    public void print()
+    {
+        for(int i = 0; i < this.rows; i++) {
+            for(int j = 0; j < this.columns; j++) {
+                System.out.print(this.matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public Matrix sum(Matrix matrix) {
+        Matrix temp = null;
+        if (this.comparison(matrix)) {
+            temp = new Matrix(this.rows, this.columns);
+            for (int i = 0; i < temp.rows; i++) {
+                for (int j = 0; j < temp.columns; j++) {
+                    temp.matrix[i][j] = this.matrix[i][j] + matrix.matrix[i][j];
+                }
+            }
+        }
+        return temp;
+    }
+    public Matrix subtraction(Matrix matrix)
+    {
+        Matrix temp = null;
+        if (this.comparison(matrix)) {
+            temp = new Matrix(this.rows, this.columns);
+            for (int i = 0; i < temp.rows; i++) {
+                for (int j = 0; j < temp.columns; j++) {
+                    temp.matrix[i][j] = this.matrix[i][j] - matrix.matrix[i][j];
+                }
+            }
+        }
+        return temp;
+    }
+
+    public Matrix multiply(Matrix matrix) {
+
+        Matrix temp = null;
+        if (this.comparison(matrix)) {
+            temp = new Matrix(this.rows, this.columns);
+            for (int i = 0; i < temp.rows; i++) {
+                for (int j = 0; j < temp.columns; j++) {
+                    temp.matrix[i][j] = this.matrix[i][j] * matrix.matrix[i][j];
+                }
+            }
+        }
+        return temp;
+    }
+    public double arithmeticMean()
+    {
+        double temp = 0;
+        int count = 0;
+
+        for(int i = 0; i < this.rows; i++) {
+            for(int j = 0; j < this.columns; j++) {
+                temp += this.matrix[i][j];
+                count++;
+            }
+        }
+        return temp/count;
+    }
+
+    public void fillManually() {
+        Scanner in = new Scanner(System.in);
+        for(int i = 0; i < this.rows; i++) {
+            System.out.printf("Введите значение [строка: %d]",i);
+            for(int j = 0; j < this.columns; j++) {
+                System.out.printf("[колонка: %d]",j);
+
+                this.matrix[i][j] = in.nextDouble();
+            }
+        }
+    }
+    public void automaticFilling(double max)
+    {
+        for(int i = 0; i < this.rows; i++) {
+            for(int j = 0; j < this.columns; j++) {
+                this.matrix[i][j] = rnd(max);
+            }
+        }
+    }
+    public double minNamber()
+    {
+        var temp = this.matrix[0][0];
+
+        for(int i = 0; i < this.rows; i++) {
+            for(int j = 0; j < this.columns; j++) {
+                if(this.matrix[i][j] < temp)
+                    temp = this.matrix[i][j];
+            }
+        }
+        return temp;
+    }
+    public double maxNamber()
+    {
+        var temp = this.matrix[0][0];
+
+        for(int i = 0; i < this.rows; i++) {
+            for(int j = 0; j < this.columns; j++) {
+                if(this.matrix[i][j] > temp)
+                    temp = this.matrix[i][j];
+            }
+        }
+        return temp;
+    }
 }
+
+
